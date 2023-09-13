59f7198a16ef1400614e5d3000aa212e86d19268
Rylie Castell <rylie.castell@gmail.com>
2023-09-13 16:16:26 +0200

    add bonus task

--- src/main/java/com/github/ryliecc/firstapi/Controller.java
+++ src/main/java/com/github/ryliecc/firstapi/Controller.java
@@ -1,16 +1,38 @@
 package com.github.ryliecc.firstapi;
 import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
+    Map<Integer, Message> messages = new HashMap<>();
 
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
     }
-    @GetMapping("/hello/{name}")
+    @GetMapping("/hello/?{name}")
     public String sayHello(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
+    @PostMapping("/messages/{message}/{name}/{id}")
+    public String greet(@PathVariable String message,@PathVariable String name, @PathVariable int id) {
+        Message newMessage = new Message(name, message, id);
+        messages.put(id, newMessage);
+        return "Message: " + message + " with the name " + name + " and ID " + id + " added to List.";
+    }
+    @GetMapping("/messages")
+    public Map<Integer, Message> listMessages() {
+        return messages;
+    }
+
+
+    @DeleteMapping("/messages/{id}")
+        public String delete(@PathVariable int id) {
+            messages.remove(id);
+            return "Message with ID  " + id + "removed.";
+
+        }
+
 }
---
+++ src/main/java/com/github/ryliecc/firstapi/Message.java
@@ -0,0 +1,13 @@
+package com.github.ryliecc.firstapi;
+
+public class Message {
+    public String name;
+    public String message;
+    public int id;
+
+    public Message(String name, String message, int id) {
+        this.name = name;
+        this.message = message;
+        this.id = id;
+    }
+}
